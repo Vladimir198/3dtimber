ec8473fbd7141f5cd99415d4af7ba273efe330fb
Владимир <vladimir.risew@yandex.ru>
2017-02-27 13:32:51 +0300

    Начало выводит в листбокс список файлов

---
+++ Unit1.dfm
@@ -0,0 +1,33 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 523
+  ClientWidth = 880
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object ListBox1: TListBox
+    Left = 8
+    Top = 32
+    Width = 185
+    Height = 483
+    ItemHeight = 13
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 16
+    Top = 1
+    Width = 75
+    Height = 25
+    Caption = #1042#1099#1073#1088#1072#1090#1100
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,44 @@
+﻿  unit Unit1;
+
+  interface
+
+  uses
+    Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+    Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, FileCtrl;
+
+  type
+    TForm1 = class(TForm)
+      ListBox1: TListBox;
+      Button1: TButton;
+      procedure Button1Click(Sender: TObject);
+    private
+      { Private declarations }
+    public
+      { Public declarations }
+    end;
+
+  var
+    Form1: TForm1;
+
+  implementation
+
+  {$R *.dfm}
+
+    procedure TForm1.Button1Click(Sender: TObject);
+        var
+        path: String;
+        sr: TSearchRec;
+    begin
+          SelectDirectory('Выбор папки', '',path);
+
+          if FindFirst(path+'\*.lprf', faAnyFile, sr)=0  then  //ищем  файлы Word  в каталоге
+
+           repeat
+               Listbox1.Items.Add(sr.Name); //выводим список в ListBox
+            until FindNext(sr)<>0;
+
+            FindClose(sr);
+    end;
+end.
+
+
